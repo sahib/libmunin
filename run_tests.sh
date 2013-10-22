0a631d41a86e5a039ef2736f123995364441e3d6
Chris P <sahib@online.de>
2013-10-22 10:50:08 +0200

    Bettre run_tests.sh script

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 # This is a poor man's solution, just grep for all occassion of unittests
+status=0
+
 for test_py in $(grep --recursive --files-with-match 'import\s*unittest' munin/)
 do
-    python ${test_py};
+    echo '--> Running:' ${test_py}
+    python ${test_py} 
+    current_status=${?}
+    echo '--> exit status was:' ${current_status}
+    if [ ${current_status} -ne 0 ] 
+        then
+            status=${current_status}
+    fi
 done
+
+echo '==> Test Exit Status is:' ${status}
+exit ${status}
