bba824f081e1f3119509dc2156e73977fc2bcbe1
Chris P <sahib@online.de>
2013-10-22 22:59:30 +0200

    Moved caching.py to session.py

--- munin/session.py
+++ munin/session.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from munin.caching import get_cache_path, check_or_mkdir
-
 from collections import Mapping
 from shutil import rmtree
 from copy import copy
@@ -19,10 +17,42 @@ import os
 :synopsis: Analyze and reanimate dead parrots.
 .. moduleauthor:: Eric Cleese <eric@python.invalid>
 .. moduleauthor:: John Idle <john@python.invalid>
-
-
-Test.
 '''
+
+try:
+    from xdg import BaseDirectory
+    HAS_XDG = True
+except ImportError:
+    HAS_XDG = False
+
+
+def check_or_mkdir(path):
+    'Check if path does exist, if not mkdir it.'
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+
+def get_cache_path(extra_name=None):
+    '''Tries to find out the XDG caching path of your system.
+
+    This is done preferrably with PyXDG. If it's not installed,
+    we try the XDG_CACHE_HOME environment variable or default to ~/.cache/
+
+    If the path does not exist yet it will be created for you.
+
+    :param extra_name: Extra path component to append to the path (or None).
+    :returns: The full path, e.g.: /home/user/.cache/libmunin/<extra_name>
+    '''
+    if HAS_XDG and 0:
+        base_dir = BaseDirectory.xdg_cache_home
+    else:
+        base_dir = os.environ.get('XDG_CACHE_HOME')
+        if base_dir is None:
+            base_dir = os.path.join(os.path.expanduser('~'), '.cache')
+
+    base_dir = os.path.join(base_dir, 'libmunin')
+    check_or_mkdir(base_dir)
+    return base_dir if not extra_name else os.path.join(base_dir, extra_name)
 
 
 class Song(Mapping):
