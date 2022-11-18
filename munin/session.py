5924f3ffb3cc841dbe7de8a908af462c9e04fa60
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-18 15:05:43 +0000

    Adding tarfile member sanitization to extractall()

--- munin/session.py
+++ munin/session.py
@@ -296,7 +296,26 @@ class Session:
         base_path, _ = os.path.splitext(full_path)
         try:
             with tarfile.open(full_path, 'r:*') as tar:
-                tar.extractall(base_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, base_path)
 
             with open(os.path.join(base_path, 'session.pickle'), 'rb') as handle:
                 return pickle.load(handle)
